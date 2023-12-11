7231e2678596963b4627abc773c985a4bc9b1b0b
Anna Ovcharenko <annaowczarenko@gmail.com>
2023-12-11 14:50:36 +0100

    Beer app

---
+++ src/main/java/guru/springframework/spring6restmvc/model/BeerStyle.java
@@ -0,0 +1,8 @@
+package guru.springframework.spring6restmvc.model;
+
+/**
+ * @author Anna Ovcharenko
+ */
+public enum BeerStyle {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
