58e3c0ffb12382aba0be111dfb2a7759b81eaec1
Anna Ovcharenko <annaowczarenko@gmail.com>
2024-01-10 12:40:15 +0100

    Add listAll and getById operations for BeerServiceJPA. Add BeerController Integration Test

--- src/main/java/guru/springframework/spring6restmvc/services/BeerServiceJPA.java
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerServiceJPA.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring6restmvc.services;
 
+import guru.springframework.spring6restmvc.entities.Beer;
 import guru.springframework.spring6restmvc.mappers.BeerMapper;
 import guru.springframework.spring6restmvc.model.BeerDTO;
 import guru.springframework.spring6restmvc.repositories.BeerRepository;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 /**
  * @author Anna Ovcharenko
@@ -25,27 +27,31 @@ public class BeerServiceJPA implements BeerService {
 
     @Override
     public List<BeerDTO> listBeers() {
-        return null;
+        return repository.findAll().stream()
+                .map(mapper::beerToBeerDto)
+                .collect(Collectors.toList());
     }
 
     @Override
     public Optional<BeerDTO> getBeerById(UUID id) {
-        return Optional.empty();
+        return Optional.ofNullable(mapper.beerToBeerDto(repository.findById(id).orElse(null)));
     }
 
     @Override
     public BeerDTO addBeer(BeerDTO beer) {
-        return null;
+        return mapper.beerToBeerDto(repository.save(mapper.beerDtoToBeer(beer)));
     }
 
     @Override
     public BeerDTO updateBeer(UUID id, BeerDTO beer) {
+//        Beer updated = repository.findById(id);
+
         return null;
     }
 
     @Override
     public void deleteBeer(UUID id) {
-
+        repository.deleteById(id);
     }
 
     @Override
